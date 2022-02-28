928cab35c81b08638a0a7a1e17dfd439d05934dc
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-28 14:09:41 +0100

    Finish exercise logging feature

--- frontend/www/scripts/automaticlogger.js
+++ frontend/www/scripts/automaticlogger.js
@@ -20,7 +20,7 @@ function onImpulse() {
 // Functions for handling on fitness selection
 async function onSitWideGripBenchPress() {
     let body = {
-        "fitnessCenter": "Sit Sit Gløshaugen idrettsbygg",
+        "fitnessCenter": "Sit Gløshaugen idrettsbygg",
         "loggedExercise": "Wide grip bench press"
     }
     let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
@@ -33,13 +33,155 @@ async function onSitWideGripBenchPress() {
 }
 
 async function onSitShortGripBenchPress() {
-    
+    let body = {
+        "fitnessCenter": "Sit Gløshaugen idrettsbygg",
+        "loggedExercise": "Short grip bench press"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
 }
 
 async function onSitWideStance() {
-    
+    let body = {
+        "fitnessCenter": "Sit Gløshaugen idrettsbygg",
+        "loggedExercise": "Wide stance"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
 }
 
 async function onSitNarrowStance() {
+    let body = {
+        "fitnessCenter": "Sit Gløshaugen idrettsbygg",
+        "loggedExercise": "Wide grip narrow stance"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
 
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
+}
+
+async function onThreeTWideGripBenchPress() {
+    let body = {
+        "fitnessCenter": "3T-Midtby'n",
+        "loggedExercise": "Wide grip bench press"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
+}
+
+async function onThreeTShortGripBenchPress() {
+    let body = {
+        "fitnessCenter": "3T-Midtby'n",
+        "loggedExercise": "Short grip bench press"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
+}
+
+async function onThreeTWideStance() {
+    let body = {
+        "fitnessCenter": "3T-Midtby'n",
+        "loggedExercise": "Wide stance"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
+}
+
+async function onThreeTNarrowStance() {
+    let body = {
+        "fitnessCenter": "3T-Midtby'n",
+        "loggedExercise": "Wide grip narrow stance"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
+}
+
+async function onImpulseWideGripBenchPress() {
+    let body = {
+        "fitnessCenter": "Impulse Treningssenter",
+        "loggedExercise": "Wide grip bench press"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
+}
+
+async function onImpulseShortGripBenchPress() {
+    let body = {
+        "fitnessCenter": "Impulse Treningssenter",
+        "loggedExercise": "Short grip bench press"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
+}
+
+async function onImpulseWideStance() {
+    let body = {
+        "fitnessCenter": "Impulse Treningssenter",
+        "loggedExercise": "Wide stance"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
+}
+
+async function onImpulseNarrowStance() {
+    let body = {
+        "fitnessCenter": "Impulse Treningssenter",
+        "loggedExercise": "Wide grip narrow stance"
+    }
+    let response = await sendRequest("POST", `${HOST}/api/exerciseLoggers/`, body)
+
+    if (response.ok) {
+        window.location.replace("automaticloggers.html")
+    } else {
+        console.log("not ok")
+    }
 }
