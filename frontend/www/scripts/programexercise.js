3b42fff4652eeb413ef260c57005dd66e47cbe7e
emil <emil.nilsen9888@gmail.com>
2022-03-01 21:47:54 +0100

    wip

---
+++ frontend/www/scripts/programexercise.js
@@ -0,0 +1,36 @@
+function onThreeTMidtbyen() {
+    document.getElementById("three-t-midtbyen-exercise-equipment").classList.remove("hide")
+    //document.getElementById("three-t-program-exercise-div").classList.remove("hide")
+}
+
+function onImpulse() {
+    document.getElementById("impulse-exercise-equipment").classList.remove("hide")
+    //document.getElementById("impulse-program-exercise-div").classList.remove("hide")
+}
+
+function onThreeTRunningMachine() {
+    document.getElementById("three-t-program-exercise-div").classList.remove("hide")
+}
+
+function onImpulseRunningMachine() {
+    document.getElementById("impulse-program-exercise-div").classList.remove("hide")
+}
+
+async function onThreeTCreateExercise() {
+    let form = document.querySelector("#three-t-running-machine-program-exercise-form")
+    let formData = new FormData(form)
+
+    let body = {
+        "terrain": formData.get("terrainGroup"),
+        "length": Number(formData.get("length")),
+        "speed": Number(formData.get("speed"))
+    }
+
+    let response = await sendRequest("POST", `${HOST}/api/`)
+
+    if (response.ok) {
+        window.location.replace("exerciseprogramming.html");
+    } else {
+        console.log("not ok")
+    }
+}
