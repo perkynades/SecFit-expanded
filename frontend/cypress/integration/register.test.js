b20188aab9e73242560f24d48282b7dc9b8b8be3
emil <emil.nilsen9888@gmail.com>
2022-03-08 19:36:51 +0100

    Add two way domain tests for register page

--- frontend/cypress/integration/register.test.js
+++ frontend/cypress/integration/register.test.js
@@ -31,3 +31,46 @@ describe('Register page boundary tests', () => {
     cy.get('li').contains('street_address').children('ul').children('li').contains('Ensure this field has no more than 50 characters.')
   })
 })
+
+describe('Register page 2-way domain tests', () => {
+  it('should accept email on both correct email and is string', () => {
+    cy.visit('http://localhost:9090/register.html')
+
+    let correctEmailString = makeid(4) + '@ntnu.no'
+
+    cy.get('input[name="username"]').type(makeid(4)+'eIsNice')
+    cy.get('input[name="email"]').type(correctEmailString)
+    cy.get('input[name="password"]').type('helloDello')
+    cy.get('input[name="password1"]').type('helloDello')
+
+    cy.get('#btn-create-account').click()
+
+    cy.wait(2000).location('pathname').should('eq', '/workouts.html')
+  })
+
+  it('should accept matching passwords that are strings', () => {
+    cy.visit('http://localhost:9090/register.html')
+
+    let password = 'nicePassBr0'
+    let password1 = password
+
+    cy.get('input[name="username"]').type(makeid(4)+'eIsNice')
+    cy.get('input[name="email"]').type(makeid(4) + '@ntnu.no')
+    cy.get('input[name="password"]').type(password)
+    cy.get('input[name="password1"]').type(password1)
+
+    cy.get('#btn-create-account').click()
+
+    cy.wait(2000).location('pathname').should('eq', '/workouts.html')
+  })
+})
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
