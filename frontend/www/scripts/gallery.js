01a028ed3afa9211fc49a9c121e6943c259b598b
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-03-28 13:06:50 +0200

    JS - Long method, temporary fields, statement nesting - dataflow

--- frontend/www/scripts/gallery.js
+++ frontend/www/scripts/gallery.js
@@ -1,89 +1,68 @@
 let goBackButton;
 
 async function retrieveWorkoutImages(id) {  
-	let workoutData = null;
 	const response = await sendRequest('GET', `${HOST}/api/workouts/${id}/`); //eslint-disable-line no-undef
 	if (!response.ok) {
 		const data = await response.json();
 		showAlert('Could not retrieve workout data!', data);
-	} else {
-		workoutData = await response.json();
-
-		getElement('workout-title').innerHTML = `Workout name: ${workoutData.name}`;
-		getElement('workout-owner').innerHTML = `Owner: ${workoutData.owner_username}`;
-
-		const hasNoImages = workoutData.files.length == 0;
-		const noImageText = getFirstElement('#no-images-text');
-
-		if(hasNoImages){
-			noImageText.classList.remove('hide');
-			return;
-		}
-
-		noImageText.classList.add('hide');
-
-        
-		const filesDiv = getElement('img-collection');
-		const filesDeleteDiv = getElement('img-collection-delete');
-        
-		const currentImageFileElement = getFirstElement('#current');
-		let isFirstImg = true;
-
-		let fileCounter = 0;
-
-		for (const file of workoutData.files) {
-			const a = document.createElement('a');
-			a.href = file.file;
-			const pathArray = file.file.split('/');
-			a.text = pathArray[pathArray.length - 1];
-			a.className = 'me-2';
-
-            
-
-			const isImage = ['jpg', 'png', 'gif', 'jpeg', 'JPG', 'PNG', 'GIF', 'JPEG'].includes(a.text.split('.')[1]);
-
-			if(isImage){
-				const deleteImgButton = document.createElement('input');
-				deleteImgButton.type = 'button';
-				deleteImgButton.className = 'btn btn-close';
-				deleteImgButton.id = file.url.split('/')[file.url.split('/').length - 2];
-				deleteImgButton.addEventListener('click', () => handleDeleteImgClick(deleteImgButton.id, 'DELETE', `Could not delete workout ${deleteImgButton.id}!`, HOST, ['jpg', 'png', 'gif', 'jpeg', 'JPG', 'PNG', 'GIF', 'JPEG'])); //eslint-disable-line no-undef
-				filesDeleteDiv.appendChild(deleteImgButton);
-                
-				const img = document.createElement('img');
-				img.src = file.file;
-                
-				filesDiv.appendChild(img);
-				deleteImgButton.style.left = `${(fileCounter % 4) * 191}px`;
-				deleteImgButton.style.top = `${Math.floor(fileCounter / 4) * 105}px`;
-
-				if(isFirstImg){
-					currentImageFileElement.src = file.file;
-					isFirstImg = false;
-				}
-				fileCounter++;
-			}
-		}
-
-		const otherImageFileElements = getFirstElement('.imgs img');
-		const selectedOpacity = 0.6;
-		otherImageFileElements[0].style.opacity = selectedOpacity;
-
-		otherImageFileElements.forEach((imageFileElement) => imageFileElement.addEventListener('click', (event) => {
-			//Changes the main image
-			currentImageFileElement.src = event.target.src;
-
-			//Adds the fade animation
-			currentImageFileElement.classList.add('fade-in');
-			setTimeout(() => currentImageFileElement.classList.remove('fade-in'), 500);
-
-			//Sets the opacity of the selected image to 0.4
-			otherImageFileElements.forEach((imageFileElement) => imageFileElement.style.opacity = 1);
-			event.target.style.opacity = selectedOpacity;
-		}));
-
+		return;
 	}
-	return workoutData;     
+
+	const workoutData = await response.json();
+
+	getElement('workout-title').innerHTML = `Workout name: ${workoutData.name}`;
+	getElement('workout-owner').innerHTML = `Owner: ${workoutData.owner_username}`;
+
+
+	if(workoutData.files.length == 0){
+		const noImageText = getFirstElement('#no-images-text');
+		noImageText.classList.remove('hide');
+		return;
+	}
+
+	const filesDiv = getElement('img-collection');
+	const filesDeleteDiv = getElement('img-collection-delete');
+        
+	const currentImageFileElement = getFirstElement('#current');
+	let isFirstImg = true;
+
+	let fileCounter = 0;
+
+	for (const file of workoutData.files) {
+		if(isFileImg(createFileLink(file))){
+			const deleteImgButton = createDeleteImgButton(file);
+			filesDeleteDiv.appendChild(deleteImgButton);
+                
+			const img = document.createElement('img');
+			img.src = file.file;
+                
+			filesDiv.appendChild(img);
+			positionDeleteImgButton(deleteImgButton, fileCounter);
+
+			if(isFirstImg){
+				currentImageFileElement.src = file.file;
+				isFirstImg = false;
+			}
+			fileCounter++;
+		}
+	}
+
+	const otherImageFileElements = getFirstElement('.imgs img');
+	const selectedOpacity = 0.6;
+	otherImageFileElements[0].style.opacity = selectedOpacity;
+
+	otherImageFileElements.forEach((imageFileElement) => imageFileElement.addEventListener('click', (event) => {
+		//Changes the main image
+		currentImageFileElement.src = event.target.src;
+
+		//Adds the fade animation
+		currentImageFileElement.classList.add('fade-in');
+		setTimeout(() => currentImageFileElement.classList.remove('fade-in'), 500);
+
+		//Sets the opacity of the selected image to 0.4
+		otherImageFileElements.forEach((imageFileElement) => imageFileElement.style.opacity = 1);
+		event.target.style.opacity = selectedOpacity;
+	}));   
 }
 
 async function validateImgFileType(id, host_variable, acceptedFileTypes) {
@@ -126,12 +105,49 @@ window.addEventListener('DOMContentLoaded', async () => {
 	await retrieveWorkoutImages(id);
 });
 
+// ---- HELPERS ----
+function createDeleteImgButton(file) {
+	const deleteImgButton =  document.createElement('input');
+	deleteImgButton.type = 'button';
+	deleteImgButton.className = 'btn btn-close';
+	deleteImgButton.id = file.url.split('/')[file.url.split('/').length - 2];
+	deleteImgButton.addEventListener(
+		'click',
+		() => {
+			handleDeleteImgClick(
+				deleteImgButton.id,
+				'DELETE',
+				`Could not delete workout ${deleteImgButton.id}!`,
+				HOST, //eslint-disable-line no-undef
+				['jpg', 'png', 'gif', 'jpeg', 'JPG', 'PNG', 'GIF', 'JPEG']
+			);
+		}
+	);
+}
+
+function positionDeleteImgButton(deleteImgButton, fileCounter) {
+	deleteImgButton.style.left = `${(fileCounter % 4) * 191}px`;
+	deleteImgButton.style.top = `${Math.floor(fileCounter / 4) * 105}px`;
+}
+
+function createFileLink(file) {
+	const a = document.createElement('a');
+	a.href = file.file;
+	const pathArray = file.file.split('/');
+	a.text = pathArray[pathArray.length - 1];
+	a.className = 'me-2';
+}
+
+function isFileImg(fileLink) {
+	return ['jpg', 'png', 'gif', 'jpeg', 'JPG', 'PNG', 'GIF', 'JPEG'].includes(fileLink.text.split('.')[1]);
+}
+
 function getElement(elementId) {
 	return document.getElementById(elementId);
 }
 
 function getFirstElement(elementId) {
-	document.querySelector(elementId);
+	return document.querySelector(elementId);
 }
 
 function showAlert(alertText, data) {
