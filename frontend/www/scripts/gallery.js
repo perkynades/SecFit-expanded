8ff508dad56710094f383dfd19ffb1f0b06f7717
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-03-28 10:58:18 +0200

    JS - duplicate code - dataflow

--- frontend/www/scripts/gallery.js
+++ frontend/www/scripts/gallery.js
@@ -5,16 +5,15 @@ async function retrieveWorkoutImages(id) {
 	const response = await sendRequest('GET', `${HOST}/api/workouts/${id}/`); //eslint-disable-line no-undef
 	if (!response.ok) {
 		const data = await response.json();
-		const alert = createAlert('Could not retrieve workout data!', data); //eslint-disable-line no-undef
-		document.body.prepend(alert);
+		showAlert('Could not retrieve workout data!', data);
 	} else {
 		workoutData = await response.json();
 
-		document.getElementById('workout-title').innerHTML = `Workout name: ${workoutData.name}`;
-		document.getElementById('workout-owner').innerHTML = `Owner: ${workoutData.owner_username}`;
+		getElement('workout-title').innerHTML = `Workout name: ${workoutData.name}`;
+		getElement('workout-owner').innerHTML = `Owner: ${workoutData.owner_username}`;
 
 		const hasNoImages = workoutData.files.length == 0;
-		const noImageText = document.querySelector('#no-images-text');
+		const noImageText = getFirstElement('#no-images-text');
 
 		if(hasNoImages){
 			noImageText.classList.remove('hide');
@@ -24,10 +23,10 @@ async function retrieveWorkoutImages(id) {
 		noImageText.classList.add('hide');
 
         
-		const filesDiv = document.getElementById('img-collection');
-		const filesDeleteDiv = document.getElementById('img-collection-delete');
+		const filesDiv = getElement('img-collection');
+		const filesDeleteDiv = getElement('img-collection-delete');
         
-		const currentImageFileElement = document.querySelector('#current');
+		const currentImageFileElement = getFirstElement('#current');
 		let isFirstImg = true;
 
 		let fileCounter = 0;
@@ -66,7 +65,7 @@ async function retrieveWorkoutImages(id) {
 			}
 		}
 
-		const otherImageFileElements = document.querySelectorAll('.imgs img');
+		const otherImageFileElements = getFirstElement('.imgs img');
 		const selectedOpacity = 0.6;
 		otherImageFileElements[0].style.opacity = selectedOpacity;
 
@@ -105,8 +104,7 @@ async function handleDeleteImgClick (id, http_keyword, fail_alert_text, host_var
 
 	if (!response.ok) {
 		const data = await response.json();
-		const alert = createAlert(fail_alert_text, data); //eslint-disable-line no-undef
-		document.body.prepend(alert);
+		showAlert(fail_alert_text, data);
 	} else {
 		location.reload();
 	}
@@ -120,10 +118,22 @@ function handleGoBackToWorkoutClick() {
 
 window.addEventListener('DOMContentLoaded', async () => {
 
-	goBackButton = document.querySelector('#btn-back-workout');
+	goBackButton = getFirstElement('#btn-back-workout');
 	goBackButton.addEventListener('click', handleGoBackToWorkoutClick);
 
 	const urlParams = new URLSearchParams(window.location.search);
 	const id = urlParams.get('id');
 	await retrieveWorkoutImages(id);
 });
+
+function getElement(elementId) {
+	return document.getElementById(elementId);
+}
+
+function getFirstElement(elementId) {
+	document.querySelector(elementId);
+}
+
+function showAlert(alertText, data) {
+	document.body.prepend(createAlert(alertText, data)); //eslint-disable-line no-undef
+}
