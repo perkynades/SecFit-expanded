05c0bdaf097f27ef124621ec62e9d9b7cb8f54f5
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-04-01 20:55:04 +0200

    users/admin - lint checks - test_permissions

--- backend/secfit/users/admin.py
+++ backend/secfit/users/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Offer, AthleteFile
 from django.contrib.auth import get_user_model
+from .models import Offer, AthleteFile
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 
-# Register your models here.
-
-
 class CustomUserAdmin(UserAdmin):
+    """
+    Class for handling the creation of an admin user
+    """
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = get_user_model()
-    # list_display = UserAdmin.list_display + ('coach',)
     fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("coach",)}),)
     add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("coach",)}),)
 
